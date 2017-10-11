303d310213bd046600804e02359d67452f4dc5d6
JerrTom <j13579x@126.com>
2017-10-11 09:46:17 +0800

    commit

---
+++ src/com/itcast/cn/JacksonTester.java
@@ -0,0 +1,49 @@
+package com.itcast.cn;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+public class JacksonTester {
+
+	public static void main(String[] args) {
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = "{\"name\":\"jinxin\",\"age\":\"25\"}";
+		try {
+			Student s = mapper.readValue(jsonString, Student.class);
+			System.out.println(s.toString());
+			String jsonstring = mapper.writeValueAsString(s);
+			System.out.println(jsonstring);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
+class Student{
+	private String name;
+	private int age;
+	//public Student() {};
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + "]";
+	}
+}
